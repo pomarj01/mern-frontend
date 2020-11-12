714d6452af44ed8ad906ae85c6b5e926a6d700b5
Jillian Pomares <pomarj@MacBook-Pro.local>
2020-11-12 22:40:45 +0000

    Add Serve library and a start script

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,8 @@ import axios from 'axios';
 // between frontend & backend 
 const api = axios.create({
   // url from the backend - 8000 for development
-  baseURL: 'http://localhost:8000/'
+  // baseURL: 'http://localhost:8000/'
+  baseURL: 'https://mern-sports-app-backend.herokuapp.com'
 })
 
 // use export default as 'const' is used
